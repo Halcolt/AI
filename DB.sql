74ec10f55168177e54a746f1bb86a227b0fef9d8
Halcolt <billtrinh19@gmail.com>
2023-05-26 12:12:14 +0700

    update table

--- DB.sql
+++ DB.sql
@@ -3,7 +3,7 @@ use AI
 CREATE TABLE movies (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(255) NOT NULL,
-  mpaa_rating VARCHAR(10),
+  MPAA_Rating varchar(255),
   budget int,
   gross int,
   release_date DATE,
@@ -11,7 +11,9 @@ CREATE TABLE movies (
   runtime INT,
   rating FLOAT,
   rating_count INT,
+  profit INT,
   PRIMARY KEY (id)
 );
 
+drop database AI
 select * from movies
