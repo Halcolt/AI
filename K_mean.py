9c39b0147bbea14e40032a51347cbd80ca41d66d
Halcolt <billtrinh19@gmail.com>
2023-05-28 13:58:52 +0700

    change algo

--- K_mean.py
+++ K_mean.py
@@ -1,7 +1,6 @@
 import mysql.connector
 import numpy as np
-from sklearn.cluster import KMeans
-from sklearn.neighbors import KNeighborsRegressor
+
 
 def connect_to_database():
     # Read MySQL config from config.txt file
@@ -25,32 +24,59 @@ def connect_to_database():
         exit()
 
 
-def predict_gross(mycursor,mydb,genre, budget, rating, runtime, k=5):
- 
+def euclidean_distance(x1, x2):
+    # Calculate the Euclidean distance between two points
+    distance = 0.0
+    for i in range(len(x1)):
+        distance += (x1[i] - x2[i]) ** 2
+    return distance
+
+def predict_gross(genre, budget, rating, runtime, k=5):
+    # Connect to MySQL database
+    mydb = connect_to_database()
+
+    # Create a cursor object to execute SQL queries
+    mycursor = mydb.cursor()
+
+    # Execute SELECT query to get movies data filtered by genre
     query = "SELECT Budget, Rating, Runtime, Gross FROM movies WHERE Genre=%s"
     mycursor.execute(query, (genre,))
 
-    # Fetch the data and convert it to a NumPy array
+    # Fetch the data
     data = mycursor.fetchall()
-    data = np.array(data)
 
-    x = data[:, :-1] # :-1 is select all row except the last one (Budget, Rating, Runtime,)
-    y = data[:, -1]  # select all data of the last collumn (Gross)
+    # Close the cursor and connection
+    mycursor.close()
+    mydb.close()
 
-    knn = KNeighborsRegressor(n_neighbors=10) #select amount of neighbor to compare
-    knn.fit(x, y) # Train the regressor on the selected features and gross
+    # Convert the data to a list
+    data = list(data)
 
-    # Make a prediction using the input features
-    input_features = [[budget, rating, runtime]]
-    predicted_gross = knn.predict(input_features).item()
+    # Calculate the Euclidean distance between the input features and each data point
+    distances = []
+    for item in data:
+        x = item[:-1]  # Features (budget, rating, runtime)
+        y = item[-1]   # Target variable (gross)
+        distance = euclidean_distance([budget, rating, runtime], x)
+        distances.append((x, y, distance))
+
+    # Sort the distances in ascending order
+    distances.sort(key=lambda x: x[2])
+
+    # Select the k nearest neighbors
+    neighbors = distances[:k]
+
+    # Calculate the average gross of the k nearest neighbors
+    total_gross = sum(neighbor[1] for neighbor in neighbors)
+    predicted_gross = total_gross / k
 
     return predicted_gross
 
 # Connect to the database
 mydb = connect_to_database()
-mycursor = mydb.cursor()
 
 # Turn off safe mode
+mycursor = mydb.cursor()
 mycursor.execute("SET SQL_SAFE_UPDATES = 0")
 mydb.commit()
 
@@ -58,16 +84,11 @@ mydb.commit()
 mycursor.execute("Update movies SET Profit = Gross-Budget")
 mydb.commit()
 
-#genre = ("Action")
-#budget = 100000
-#rating = 7.5
-#runtime = 120
-
 genre = input("Genre: ") 
 budget = int(input("Budget: "))
 rating = float(input("Rating: "))
 runtime = int(input("runtime: "))
-predicted_gross = predict_gross(mycursor,mydb, genre, budget, rating, runtime)
+predicted_gross = predict_gross(genre, budget, rating, runtime)
 print(f"The predicted gross for a movie with genre={genre}, budget={budget}, rating={rating}, and runtime={runtime} days, is around ${predicted_gross:.2f}")
 
 # Close the cursor and connection
